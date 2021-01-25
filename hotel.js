97561d5d9012b48974f5452ef55788caf98ec02d
Tony Long <61607926+tonylong1314520@users.noreply.github.com>
2021-01-25 01:06:51 -1000

    Update hotel.js

--- hotel.js
+++ hotel.js
@@ -1,102 +1,44 @@
-var cartTotals = [];
-var clickAmount2 = 0;
-var clickAmount3 = 0;
-var clickAmount4 = 0;
 function addOne() {
+	var totals = 0;
   var x = document.getElementById('roomOne').value;
+  document.getElementById('roomOne').value = "";
   var d = new Date();
   var y = x + " added on " + d;
   var newItem = document.createElement('li');
-  document.getElementById('roomOne').value = "";
   var newText = document.createTextNode(y);
-  if (clickAmount2 < 3) {
-    clickAmount2++;
   newItem.appendChild(newText);
   var position = document.getElementById('listOne');
   position.appendChild(newItem);
-  cartTotals.push(100);
-  addTotal();
-  }
-  window.alert("$100 has been added to your bill, remove to refund your submission.");
+  totals = x * 100;
+  document.getElementById('grandTotal').innerHTML = "4.712% tax: $ " + ((totals * 1.04712).toFixed(2));
+  window.alert(x + " guests has been added to your bill.");
 }
+//unfinished function
 function removeOne() {
-  var thing = document.getElementById('removeOne').value;
-  var x = thing - 1;
-  var removeItem = document.getElementById('listOne').getElementsByTagName('li')[x];
-  document.getElementById('removeOne').value = "";
-  var containerElement = removeItem.parentNode;
-  containerElement.removeChild(removeItem);
-  cartTotals.push(-100);
-  addTotal();
-  clickAmount2--;
-  window.alert("You have been refunded your full price.");
+	let totals = 0;
+  let num = [];
+	let x = 0;
+    window.alert(document.getElementById('grandTotal').value);
+  if (document.getElementById('removeOne').value != "") {
+  	x = document.getElementById('removeOne').value;
+    document.getElementById('removeOne').value = "";
+  } else if (document.getElementById('listTwo').value != "") {
+  	x = document.getElementById('listTwo').value;
+    document.getElementById('listTwo').value = "";
+  } else {
+  	x = document.getElementById('listThree').value;
+    document.getElementById('listThree').value = "";
+  }
+  let y = document.getElementById('grandTotal').innerHTML;
+  for(let i = 0; i < y.length; i++) {
+  	if(y[i] >= 0) {
+    	num.push(y[i]);
+    }
+  } 
+  document.getElementById('grandTotal').innerHTML = "4.712% tax: $ " + ((totals * 1.04712).toFixed(2));
+  window.alert("You have removed guests.");
 }
 
-function addTwo() {
-  var x = document.getElementById('roomTwo').value;
-  var d = new Date();
-  var y = x + " added on " + d;
-  var newItem = document.createElement('li');
-  document.getElementById('roomTwo').value = "";
-  var newText = document.createTextNode(y);
-  if (clickAmount3 < 3) {
-    clickAmount3++;
-  newItem.appendChild(newText);
-  var position = document.getElementById('listTwo');
-  position.appendChild(newItem);
-  cartTotals.push(100);
-  addTotal();
-  }
-  window.alert("$100 has been added to your bill, remove to refund your submission.");
-}
-function removeTwo() {
-  var thing = document.getElementById('removeTwo').value;
-  var x = thing - 1;
-  var removeItem = document.getElementById('listTwo').getElementsByTagName('li')[x];
-  document.getElementById('removeTwo').value = "";
-  var containerElement = removeItem.parentNode;
-  containerElement.removeChild(removeItem);
-  cartTotals.push(-100);
-  addTotal();
-  clickAmount3--;
-  window.alert("You have been refunded your full price.");
-}
-function addThree() {
-  var x = document.getElementById('roomThree').value;
-  var d = new Date();
-  var y = x + " added on " + d;
-  var newItem = document.createElement('li');
-  document.getElementById('roomThree').value = "";
-  var newText = document.createTextNode(y);
-  if (clickAmount4 < 3) {
-    clickAmount4++;
-  newItem.appendChild(newText);
-  var position = document.getElementById('listThree');
-  position.appendChild(newItem);
-  cartTotals.push(100);
-  addTotal();
-  }
-  window.alert("$100 has been added to your bill, remove to refund your submission.");
-}
-function removeThree() {
-  var thing = document.getElementById('removeThree').value;
-  var x = thing - 1;
-  var removeItem = document.getElementById('listThree').getElementsByTagName('li')[x];
-  document.getElementById('removeThree').value = "";
-  var containerElement = removeItem.parentNode;
-  containerElement.removeChild(removeItem);
-  cartTotals.push(-100);
-  addTotal();
-  clickAmount4--;
-  window.alert("You have been refunded your full price.");
-}
-function addTotal() {
-  var total = 0;
-  for (var i = 0; i < cartTotals.length; i++) {
-   var lmao = total += cartTotals[i];
-  document.getElementById('grandTotal').innerHTML = "4.712% tax: $ " + ((total * 1.04712).toFixed(2));
-}
-}
 function creditCard() {
   var youtube = document.getElementById('credit').value;
   document.getElementById('credit').value = "";
@@ -104,6 +46,6 @@ function creditCard() {
   document.getElementById('outcome').innerHTML = "The total above" + " is processed to the account # " + (youtube);
   }
   else {
-    window.alert('INVALID CREDIT CARD NUMBER (16 digit credit card number)');
+    window.alert('INVALID CREDIT CARD NUMBER');
   }
 }
